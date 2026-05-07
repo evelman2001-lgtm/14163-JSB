84063478a5df2d9e3954fc61e4c6a2fe5c101e61
evelman2001-lgtm <evelman2001@gmail.com>
2026-05-07 12:10:52 +0900

    sbb로 클래스명 변경

--- src/main/java/com/back/sbb/sbbApplication.java
+++ src/main/java/com/back/sbb/sbbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JbsApplication {
+public class sbbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JbsApplication.class, args);
+        SpringApplication.run(sbbApplication.class, args);
     }
 
 }
