98cde6e830ce01460797c556ec6ca0a5ba1c2b78
evelman2001-lgtm <evelman2001@gmail.com>
2026-05-07 12:08:04 +0900

    JBS 세팅

---
+++ src/main/java/com/back/sbb/JbsApplication.java
@@ -0,0 +1,13 @@
+package com.back.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbsApplication.class, args);
+    }
+
+}
